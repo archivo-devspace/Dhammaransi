03f3f1291461e8aa9cda527a0ccfb4f9831465f6
HtetThuYaAungg <htetthuya.htya@gmail.com>
2024-04-12 09:54:05 +0630

    [ENHANCEMENT] initial project setup

--- ios/DhammaransiAppTests/DhammaransiAppTests.m
+++ ios/DhammaransiAppTests/DhammaransiAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface DhammaransiAppTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation DhammaransiAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
