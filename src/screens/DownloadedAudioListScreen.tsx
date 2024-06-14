859753c451c7c61e866f3271e046737fb6a67a1c
HtetThuYaAungg <htetthuya.htya@gmail.com>
2024-06-14 13:59:10 +0630

    [ENHANCEMENT] UI Update at DownloadedListScreen

--- src/screens/DownloadedAudioListScreen.tsx
+++ src/screens/DownloadedAudioListScreen.tsx
@@ -8,9 +8,11 @@ import {
   useWindowDimensions,
   FlatList,
   TouchableOpacity,
+  Modal,
+  SafeAreaView,
+  StatusBar,
 } from 'react-native';
-import React, {useEffect, useLayoutEffect, useState} from 'react';
-
+import React, {useEffect, useState, useCallback, useRef} from 'react';
 import {useIsFocused} from '@react-navigation/core';
 import RNFetchBlob from 'rn-fetch-blob';
 import {
@@ -36,208 +38,225 @@ const OfflineDownloadGrid = ({navigation}: Props) => {
   const {deleteTrackById, currentTrack, handlePlay, setTrackLists} =
     useTrackContext();
   const {t} = useTranslation();
-  const [data, setData] = useState<any>([]);
-  const [reRender, setReRender] = useState(false);
+  const [tracks, setTracks] = useState<any>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [selectedTrackId, setSelectedTrackId] = useState(null);
+  const swipeableRef = useRef<Swipeable | null>(null);
 
   const isFocused = useIsFocused();
   const {theme} = useThemeContext();
   const {height, width} = useWindowDimensions();
-  const styles = styling(theme);
+  const styles = createStyles(theme);
 
-  // let TrackFolder =
-  //   Platform.OS === 'android'
-  //     ? RNFetchBlob.fs.dirs.CacheDir
-  //     : RNFetchBlob.fs.dirs.DocumentDir;
-
-  const fetchDownloadedData = async () => {
+  const fetchDownloadedData = useCallback(async () => {
     setIsLoading(true);
     try {
-      await fetchDownloadedDataFromLocalDir(async item => {
-        const sortedData = item.sort((a: any, b: any) => {
-          const dateA = new Date(a.downloadDate) as any;
-          const dateB = new Date(b.downloadDate) as any;
-          return dateB - dateA;
-        });
-        const updatedData = await Promise.all(
-          sortedData.map(async (item: any) => {
-            const filePath = item.artwork;
-            const fileExists = await RNFetchBlob.fs.exists(filePath);
+      await fetchDownloadedDataFromLocalDir(async items => {
+        const sortedItems = items.sort(
+          (a: any, b: any) =>
+            new Date(b.downloadDate).getTime() -
+            new Date(a.downloadDate).getTime(),
+        );
+        const updatedItems = await Promise.all(
+          sortedItems.map(async (item: any) => {
+            const fileExists = await RNFetchBlob.fs.exists(item.artwork);
             return {
               ...item,
               artwork: fileExists
                 ? Platform.OS === 'android'
-                  ? `file://${filePath}`
-                  : filePath
+                  ? `file://${item.artwork}`
+                  : item.artwork
                 : '',
             };
           }),
         );
-
-        setData(updatedData);
-        setTrackLists(updatedData);
+        setTracks(updatedItems);
+        setTrackLists(updatedItems);
       });
-      setIsLoading(false); // Moved outside the try block
     } catch (error) {
       console.error('Error fetching data:', error);
+    } finally {
       setIsLoading(false);
     }
-  };
+  }, [setTrackLists]);
 
-  const handlePlayAudio = async (item: any) => {
-    // togglePlayingMode();
-    if (currentTrack === null || currentTrack.id !== item.id) {
+  const handlePlayAudio = (item: any) => {
+    if (!currentTrack || currentTrack.id !== item.id) {
       handlePlay(item.id);
     }
     navigation.navigate('TrackBottom');
   };
 
-  const onDeletionPress = async (id: any) => {
-    await deleteContentFromLocalDir(id);
-    deleteTrackById(id);
-    await fetchDownloadedData();
+  const confirmDeletion = (id: any) => {
+    setSelectedTrackId(id);
+    setModalVisible(true);
+  };
+
+  const handleDeletion = async () => {
+    if (selectedTrackId) {
+      await deleteContentFromLocalDir(selectedTrackId);
+      deleteTrackById(selectedTrackId);
+      fetchDownloadedData();
+      setModalVisible(false);
+      setSelectedTrackId(null);
+    }
+  };
+
+  const handleCancelDeletion = () => {
+    setModalVisible(false);
+    if (swipeableRef.current) {
+      swipeableRef.current.close();
+    }
   };
 
   useEffect(() => {
-    // Only fetch data if either reRender or isFocused changes
-    if (reRender || isFocused) {
+    if (isFocused) {
       fetchDownloadedData();
     }
-  }, [reRender, isFocused]);
+  }, [isFocused, fetchDownloadedData]);
 
   useEffect(() => {
     const downloadListenerStatus = DeviceEventEmitter.addListener(
       'downloadDone',
-      e => {
-        setReRender(true);
+      () => {
+        fetchDownloadedData();
       },
     );
     return () => {
       downloadListenerStatus.remove();
     };
-  }, []);
+  }, [fetchDownloadedData]);
 
-  // const handlePlay = (url: any, thumbnail: any, item: any) => {
-  //   const finalUrl = url.split('/').pop();
-  //   const offlineUrl = TrackFolder + '/' + finalUrl;
-
-  //   const thumbnailImage =
-  //     Platform.OS === 'android' ? 'file://' + thumbnail : thumbnail;
-
-  //   navigation.navigate('PlayerScreen', {
-  //     source: offlineUrl,
-  //     posterImage: thumbnailImage,
-  //     data: item,
-  //     offline: true,
-  //   });
-  // };
-
-  const rightSwipe = (id: any) => {
-    return (
-      <TouchableOpacity
-        onPress={() => onDeletionPress(id)}
-        style={{
-          justifyContent: 'center',
-          paddingHorizontal: 10,
-          zIndex: 10,
-          backgroundColor: Colors[theme].danger,
-        }}>
-        <AntDesign name={'delete'} size={30} color={'white'} />
-      </TouchableOpacity>
-    );
-  };
-
-  let renderItem;
-  if (isLoading) {
-    renderItem = (
-      <View style={styles.noDataConatiner}>
-        <LoadingSpinner
-          durationMs={1500}
-          loaderSize={70}
-          bgColor={Colors[theme].secondary_dark}
-          color={Colors[theme].primary_light}
-          loadingText="LOADING"
-          loadingTextColor={Colors[theme].primary}
-          loadingTextSize={8}
-        />
+  const renderTrackItem = ({item}: {item: any}) => (
+    <Swipeable
+      ref={swipeableRef}
+      // onSwipeableRightOpen={() => confirmDeletion(item.id)}
+      // onSwipeableOpenStartDrag={() => confirmDeletion(item.id)}
+      onSwipeableWillOpen={() => confirmDeletion(item.id)}
+      // onSwipeableCloseStartDrag={() => confirmDeletion(item.id)}
+      // onSwipeableCloseStartDrag={() => confirmDeletion(item.id)}
+      renderLeftActions={() => (
+        <View style={styles.leftDeleteContainer}>
+          <View style={styles.deleteButton}>
+            <AntDesign name="delete" size={30} color="white" />
+          </View>
+        </View>
+      )}
+      renderRightActions={() => (
+        <View style={styles.deleteContainer}>
+          <View style={styles.deleteButton}>
+            <AntDesign name="delete" size={30} color="white" />
+          </View>
+        </View>
+      )}>
+      <View style={styles.trackItem}>
+        <View style={styles.trackDetails}>
+          <Image
+            source={{uri: item.artwork}}
+            resizeMode="cover"
+            style={styles.trackImage}
+          />
+          <View style={styles.trackTextContainer}>
+            <Text style={styles.trackTitle}>
+              {truncateText(item.title, 45)}
+            </Text>
+            <Text style={styles.trackArtist}>
+              {truncateText(item.artist, 30)}
+            </Text>
+          </View>
+        </View>
+        <CustomButton
+          onPress={() => handlePlayAudio(item)}
+          customButtonStyle={styles.playButton}>
+          <AntDesign
+            name="caretright"
+            size={30}
+            color={Colors[theme].primary}
+          />
+        </CustomButton>
       </View>
-    );
-  } else if (!isLoading && data.length === 0) {
-    renderItem = (
-      <View style={styles.noDataConatiner}>
-        <Image
-          source={require('../assets/nodata.png')}
-          style={{
-            width: width * 0.8,
-            height: height * 0.5,
-            borderRadius: 100,
-          }}
-          resizeMode="stretch"
-        />
-        <Text style={styles.noDataText}>{t('UTILS.NODOWNLOADDATA')}</Text>
-      </View>
-    );
-  } else if (!isLoading && data.length >= 0) {
-    renderItem = (
-      <FlatList
-        data={data}
-        showsVerticalScrollIndicator={false}
-        renderItem={({item}) => {
-          console.log('id', item.id);
-          return (
-            <Swipeable
-              key={item.id}
-              renderRightActions={() => rightSwipe(item.id)}>
-              <View style={styles.container}>
-                <View style={styles.trackContainer}>
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      gap: 16,
-                      alignItems: 'center',
-                    }}>
-                    <Image
-                      source={{uri: item.artwork}}
-                      resizeMode="cover"
-                      style={styles.img}
-                    />
-                    <View style={{width: '70%', gap: 10}}>
-                      <Text style={styles.title}>
-                        {truncateText(item.title, 45)}
-                      </Text>
-                      <Text style={styles.desc}>
-                        {truncateText(item.artist, 30)}
-                      </Text>
-                    </View>
-                  </View>
-                  <CustomButton
-                    onPress={() => handlePlayAudio(item)}
-                    customButtonStyle={styles.btn}>
-                    <AntDesign
-                      name={'caretright'}
-                      size={30}
-                      color={Colors[theme].primary}
-                      style={{backgroundColor: 'transparent'}}
-                    />
-                  </CustomButton>
-                </View>
-              </View>
-            </Swipeable>
-          );
-        }}
-        ItemSeparatorComponent={() => <View style={styles.divider} />}
-        keyExtractor={(item: any) => item.id.toString()}
-      />
-    );
-  }
-  return <Container title="TITLES.DOWNLOADAUDOIS">{renderItem}</Container>;
+    </Swipeable>
+  );
+
+  return (
+    <>
+      <Container title={t('TITLES.DOWNLOADAUDOIS')}>
+        {isLoading ? (
+          <View style={styles.loadingContainer}>
+            <LoadingSpinner
+              durationMs={1500}
+              loaderSize={70}
+              bgColor={Colors[theme].secondary_dark}
+              color={Colors[theme].primary_light}
+              loadingText={t('UTILS.LOADING')}
+              loadingTextColor={Colors[theme].primary}
+              loadingTextSize={8}
+            />
+          </View>
+        ) : tracks.length === 0 ? (
+          <View style={styles.noDataContainer}>
+            <Image
+              source={require('../assets/nodata.png')}
+              style={{
+                width: width * 0.8,
+                height: height * 0.5,
+                borderRadius: 100,
+              }}
+              resizeMode="stretch"
+            />
+            <Text style={styles.noDataText}>{t('UTILS.NODOWNLOADDATA')}</Text>
+          </View>
+        ) : (
+          <FlatList
+            data={tracks}
+            showsVerticalScrollIndicator={false}
+            renderItem={renderTrackItem}
+            ItemSeparatorComponent={() => <View style={styles.divider} />}
+            keyExtractor={item => item.id.toString()}
+          />
+        )}
+      </Container>
+
+      <Modal
+        transparent={true}
+        animationType="fade"
+        visible={isModalVisible}
+        onRequestClose={() => setModalVisible(false)}>
+        <StatusBar backgroundColor={'rgba(0, 0, 0, 0.5)'} />
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>{t('UTILS.DELETE')}</Text>
+            <View style={styles.modalActions}>
+              <TouchableOpacity
+                onPress={handleDeletion}
+                style={styles.confirmButton}>
+                <Text style={styles.buttonText}>{t('UTILS.YES')}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={handleCancelDeletion}
+                style={styles.cancelButton}>
+                <Text style={styles.buttonText}>{t('UTILS.NO')}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </>
+  );
 };
 
 export default OfflineDownloadGrid;
 
-const styling = (theme: Theme) =>
+const createStyles = (theme: Theme) =>
   StyleSheet.create({
-    noDataConatiner: {
+    loadingContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+      gap: 20,
+    },
+    noDataContainer: {
       justifyContent: 'center',
       alignItems: 'center',
       flex: 1,
@@ -248,44 +267,105 @@ const styling = (theme: Theme) =>
       fontWeight: 'bold',
       fontSize: 20,
     },
-    container: {
+    trackItem: {
       flexDirection: 'row',
       justifyContent: 'space-between',
       alignItems: 'center',
       paddingVertical: 20,
       paddingHorizontal: 20,
-      backgroundColor: Colors[theme]?.secondary,
+      backgroundColor: Colors[theme].secondary,
     },
-    trackContainer: {
+    trackDetails: {
       flexDirection: 'row',
-      justifyContent: 'space-between',
+      gap: 16,
       alignItems: 'center',
-      width: '100%',
-      paddingRight: 20,
-      height: 60,
     },
-    img: {
+    trackImage: {
       width: 60,
       height: 60,
       borderRadius: 12,
     },
-    btn: {
-      backgroundColor: 'transparent',
+    trackTextContainer: {
+      width: '70%',
+      gap: 10,
     },
-    title: {
+    trackTitle: {
       fontSize: 16,
       color: Colors[theme].text,
     },
-    desc: {
+    trackArtist: {
       fontSize: 12,
       color: Colors[theme].text,
+    },
+    playButton: {
+      backgroundColor: 'transparent',
+    },
+    deleteContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+      backgroundColor: Colors[theme].danger,
+    },
+    leftDeleteContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+      backgroundColor: Colors[theme].danger,
+    },
+    deleteButton: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 70,
+      height: '100%',
     },
     divider: {
       width: '100%',
       height: 1,
       backgroundColor: Colors[theme].secondary_dark,
     },
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+      width: '80%',
+      backgroundColor: Colors[theme].secondary_dark,
+      padding: 20,
+      borderRadius: 10,
+      alignItems: 'center',
+    },
+    modalText: {
+      fontSize: 18,
+      textAlign: 'center',
+      lineHeight: 35,
+      color: Colors[theme].text,
+      marginBottom: 20,
+    },
+    modalActions: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '100%',
+    },
+    confirmButton: {
+      backgroundColor: Colors[theme].danger,
+      padding: 10,
+      borderRadius: 5,
+      flex: 1,
+      alignItems: 'center',
+      marginRight: 10,
+    },
+    cancelButton: {
+      backgroundColor: Colors[theme].primary,
+      padding: 10,
+      borderRadius: 5,
+      flex: 1,
+      alignItems: 'center',
+      marginLeft: 10,
+    },
+    buttonText: {
+      color: 'white',
+      fontSize: 16,
+    },
   });
-function setTrackLists(data: any) {
-  throw new Error('Function not implemented.');
-}
