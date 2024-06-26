ff1af8319ca2749932a3a402e8e550b6af6d3561
Hein Thant Zaw <heinthantzawheinthantzaw@abank.com.mm>
2024-06-26 16:27:24 +0630

    [ENHANCEMENT]Create Missionary Detail Screen

--- src/screens/MissionDetailScreen.tsx
+++ src/screens/MissionDetailScreen.tsx
@@ -1,7 +1,17 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {NavigationMainBottomTabScreenProps} from '../navigations/BottomNavigation';
+import {MainStackParamList} from '../navigations/StackNavigation';
+import {RouteProp} from '@react-navigation/native';
 
-const MissionDetailScreen = () => {
+type Props = {
+  route: RouteProp<MainStackParamList, 'MissionDetail'>;
+  navigation: NavigationMainBottomTabScreenProps['navigation'];
+};
+
+const MissionDetailScreen = ({navigation, route}: Props) => {
+  const {id} = route.params;
+  console.log('id', id);
   return (
     <View>
       <Text>MissionDetailScreen</Text>
