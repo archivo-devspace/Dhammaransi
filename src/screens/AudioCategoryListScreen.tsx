26b8f053f07007939031b7945c9a01b69233ba1f
HeinThantZaw28 <heinthantzaw52@gmail.com>
2024-09-28 23:48:40 +0630

    [MODIFY]Add Loading Indicator to the album list api binding

--- src/screens/AudioCategoryListScreen.tsx
+++ src/screens/AudioCategoryListScreen.tsx
@@ -5,10 +5,11 @@ import {AntDesign, truncateText} from '../utils/common';
 import {CustomButton} from '../components/utils';
 import Container from '../components/commons/Container';
 import {Theme, useThemeContext} from '../contexts/ThemeContext';
-import {trackCategories} from '../utils/constants';
 import {Colors} from '../theme';
 import {NavigationMainStackScreenProps} from '../navigations/StackNavigation';
 import {useGetAlbums} from '../api_services/lib/queryhooks/useAudio';
+import LoadingSpinner from '../components/utils/LoadingSpinner';
+import {useTranslation} from 'react-i18next';
 
 type Props = {
   navigation: NavigationMainStackScreenProps['navigation'];
@@ -17,6 +18,7 @@ type Props = {
 const AudioCategoryListScreen = ({navigation}: Props) => {
   const {theme} = useThemeContext();
   const styles = styling(theme);
+  const {t} = useTranslation();
   const {data: albums, isLoading: isAlbumsLoading} = useGetAlbums();
 
   const handleNavigate = (id: number) => {
@@ -24,52 +26,66 @@ const AudioCategoryListScreen = ({navigation}: Props) => {
   };
   return (
     <Container title="TITLES.AUDIOCATEGORIES">
-      <FlatList
-        data={albums?.data.results.data}
-        showsVerticalScrollIndicator={false}
-        renderItem={({item}) => (
-          <React.Fragment key={item.id}>
-            <View style={styles.container}>
-              <CustomButton
-                onPress={() => handleNavigate(item.id)}
-                customButtonStyle={styles.btn}>
-                <View style={styles.trackContainer}>
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      gap: 16,
-                      alignItems: 'center',
-                    }}>
-                    <Image
-                      source={{
-                        uri: 'https://upload.wikimedia.org/wikipedia/commons/4/44/Paticcasamuppada_Burmese.jpg',
-                      }}
-                      resizeMode="cover"
-                      style={styles.img}
-                    />
-                    <View style={{width: '70%', gap: 10}}>
-                      <Text style={styles.title}>
-                        {truncateText(item.title, 45)}
-                      </Text>
-                      <Text style={styles.desc}>
-                        {truncateText(item.status, 30)}
-                      </Text>
+      {isAlbumsLoading ? (
+        <View style={styles.loadingContainer}>
+          <LoadingSpinner
+            durationMs={1500}
+            loaderSize={50}
+            bgColor={Colors[theme].secondary_dark}
+            color={Colors[theme].primary_light}
+            loadingText={t('UTILS.LOADING')}
+            loadingTextColor={Colors[theme].primary}
+            loadingTextSize={4}
+          />
+        </View>
+      ) : (
+        <FlatList
+          data={albums?.data.results.data}
+          showsVerticalScrollIndicator={false}
+          renderItem={({item}) => (
+            <React.Fragment key={item.id}>
+              <View style={styles.container}>
+                <CustomButton
+                  onPress={() => handleNavigate(item.id)}
+                  customButtonStyle={styles.btn}>
+                  <View style={styles.trackContainer}>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        gap: 16,
+                        alignItems: 'center',
+                      }}>
+                      <Image
+                        source={{
+                          uri: 'https://upload.wikimedia.org/wikipedia/commons/4/44/Paticcasamuppada_Burmese.jpg',
+                        }}
+                        resizeMode="cover"
+                        style={styles.img}
+                      />
+                      <View style={{width: '70%', gap: 10}}>
+                        <Text style={styles.title}>
+                          {truncateText(item.title, 45)}
+                        </Text>
+                        <Text style={styles.desc}>
+                          {truncateText(item.status, 30)}
+                        </Text>
+                      </View>
                     </View>
+                    <AntDesign
+                      name={'arrowright'}
+                      size={30}
+                      color={Colors[theme].primary}
+                    />
                   </View>
-                  <AntDesign
-                    name={'arrowright'}
-                    size={30}
-                    color={Colors[theme].primary}
-                  />
-                </View>
-              </CustomButton>
-            </View>
-          </React.Fragment>
-        )}
-        ItemSeparatorComponent={() => <View style={styles.divider} />}
-        keyExtractor={item => item.id.toString()}
-        // Optional: Add extra FlatList props like `ItemSeparatorComponent`, etc.
-      />
+                </CustomButton>
+              </View>
+            </React.Fragment>
+          )}
+          ItemSeparatorComponent={() => <View style={styles.divider} />}
+          keyExtractor={item => item.id.toString()}
+          // Optional: Add extra FlatList props like `ItemSeparatorComponent`, etc.
+        />
+      )}
     </Container>
   );
 };
@@ -84,6 +100,12 @@ const styling = (theme: Theme) =>
       alignItems: 'center',
       paddingVertical: 20,
       paddingHorizontal: 20,
+    },
+    loadingContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+      gap: 20,
     },
     trackContainer: {
       gap: 5,
