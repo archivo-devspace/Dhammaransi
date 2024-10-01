1a404fa64ec91a6dd8f93d86d1622aabf2fd314c
HeinThantZaw28 <heinthantzaw52@gmail.com>
2024-10-01 21:28:48 +0630

    [REFACTOR]Remove Console log

--- src/api_services/lib/services/BooksService.ts
+++ src/api_services/lib/services/BooksService.ts
@@ -2,6 +2,5 @@ import axiosInstance from '../AxiosInstance';
 
 export const fetchBooksList = async () => {
   const res = await axiosInstance.get('/books');
-  console.log("data")
   return res.data;
 };
