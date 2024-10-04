27ac851c85ca5b77ecd2b589d7fdf3f4d2412d43
HeinThantZaw28 <heinthantzaw52@gmail.com>
2024-10-04 23:11:20 +0630

    [ENHANCEMENT]Change Icon in Data Not Found Component

--- src/components/commons/DataNotFound/index.tsx
+++ src/components/commons/DataNotFound/index.tsx
@@ -10,7 +10,7 @@ import {
 import React from 'react';
 import {Theme, useThemeContext} from '../../../contexts/ThemeContext';
 import {Colors} from '../../../theme';
-import {AntDesign, remToPx} from '../../../utils/common';
+import {AntDesign, Feather, remToPx} from '../../../utils/common';
 import {useTranslation} from 'react-i18next';
 
 export interface DataNotFoundProps {
@@ -25,11 +25,19 @@ const DataNotFound = ({customStyle, error}: DataNotFoundProps) => {
   const styles = styling(theme);
   return (
     <View style={[styles.contiainer, customStyle]}>
-      <AntDesign
-        name={'frowno'}
-        size={height * 0.04}
-        color={Colors[theme].primary}
-      />
+      {error ? (
+        <Feather
+          name="wifi-off"
+          size={height * 0.04}
+          color={Colors[theme].primary}
+        />
+      ) : (
+        <AntDesign
+          name={'frowno'}
+          size={height * 0.04}
+          color={Colors[theme].primary}
+        />
+      )}
       <Text style={[styles.text, {fontSize: height * 0.021}]}>
         {error ? t('UTILS.FETCH_FAILED') : t('UTILS.NO_DATA_AVAILABLE')}
       </Text>
